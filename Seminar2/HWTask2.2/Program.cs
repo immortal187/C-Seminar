8660d6c110cd1159d649286904905782d9d4bc97
3eLeHblu <innocent87@mail.ru>
2023-07-01 16:35:31 +0300

    Семинар2 + ДЗ

---
+++ Seminar2/HWTask2.2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a < 100 && a > -100)
+{
+    Console.Write("Третьего числа нет");
+}
+else
+{
+    while (a > 999 || a < -999)
+    {
+        a = a / 10;
+    }
+
+   Console.Write("Третья цифра числа: " + Math.Abs(a%10)); 
+}
--- Seminar2/HWTask2/Program.cs
+++ Seminar2/HWTask2/Program.cs
@@ -1,1 +1,33 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int GetThirdRank (int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool ValidateNumber (int number)   
+{
+    if (number < 100)
+    {
+        Console.WriteLine("3 нет");
+        return false;
+    }
+    return true;
+}
+int number = Promt("Введите число: ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(GetThirdRank(number));  
+}
